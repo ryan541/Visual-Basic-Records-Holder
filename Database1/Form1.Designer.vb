80e8be3d5c91435a50a214606b41500b4794eb8e
ryanmwaniki <ryanmwaniki60@gmail.com>
2022-03-19 18:41:14 +0300

    Database Holder

---
+++ Database1/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.txtFullnames = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtBusinessName = New System.Windows.Forms.TextBox()
+        Me.txtLocation = New System.Windows.Forms.TextBox()
+        Me.txtCreditlimit = New System.Windows.Forms.TextBox()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.cmdDelete = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Full Names"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 208)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Business Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 269)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Location"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(33, 322)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Credit Limit"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.Location = New System.Drawing.Point(145, 44)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(204, 20)
+        Me.txtCustomerID.TabIndex = 6
+        '
+        'txtFullnames
+        '
+        Me.txtFullnames.Location = New System.Drawing.Point(145, 94)
+        Me.txtFullnames.Name = "txtFullnames"
+        Me.txtFullnames.Size = New System.Drawing.Size(204, 20)
+        Me.txtFullnames.TabIndex = 7
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(145, 145)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(204, 20)
+        Me.txtAddress.TabIndex = 8
+        '
+        'txtBusinessName
+        '
+        Me.txtBusinessName.Location = New System.Drawing.Point(145, 205)
+        Me.txtBusinessName.Name = "txtBusinessName"
+        Me.txtBusinessName.Size = New System.Drawing.Size(204, 20)
+        Me.txtBusinessName.TabIndex = 9
+        '
+        'txtLocation
+        '
+        Me.txtLocation.Location = New System.Drawing.Point(145, 266)
+        Me.txtLocation.Name = "txtLocation"
+        Me.txtLocation.Size = New System.Drawing.Size(204, 20)
+        Me.txtLocation.TabIndex = 10
+        '
+        'txtCreditlimit
+        '
+        Me.txtCreditlimit.Location = New System.Drawing.Point(145, 322)
+        Me.txtCreditlimit.Name = "txtCreditlimit"
+        Me.txtCreditlimit.Size = New System.Drawing.Size(204, 20)
+        Me.txtCreditlimit.TabIndex = 11
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(578, 137)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(133, 28)
+        Me.cmdSave.TabIndex = 12
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'cmdDelete
+        '
+        Me.cmdDelete.Location = New System.Drawing.Point(578, 221)
+        Me.cmdDelete.Name = "cmdDelete"
+        Me.cmdDelete.Size = New System.Drawing.Size(133, 28)
+        Me.cmdDelete.TabIndex = 13
+        Me.cmdDelete.Text = "Delete"
+        Me.cmdDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmdDelete)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.txtCreditlimit)
+        Me.Controls.Add(Me.txtLocation)
+        Me.Controls.Add(Me.txtBusinessName)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtFullnames)
+        Me.Controls.Add(Me.txtCustomerID)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtCustomerID As TextBox
+    Friend WithEvents txtFullnames As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtBusinessName As TextBox
+    Friend WithEvents txtLocation As TextBox
+    Friend WithEvents txtCreditlimit As TextBox
+    Friend WithEvents cmdSave As Button
+    Friend WithEvents cmdDelete As Button
+End Class
---
+++ Database1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Dim pro As String
+    Dim conString As String
+    Dim command As String
+    Dim myconnection As OleDbConnection = New OleDbConnection
+
+    Private Sub cmdSave_Click(sender As Object, e As EventArgs) Handles cmdSave.Click
+        pro = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\user\Documents\Customer.mdb"
+        conString = pro
+        myconnection.ConnectionString = conString
+        myconnection.Open()
+        command = "Insert into Customer ([CustomerID],[Full Names],[Address],[Business Name],[Location],[Credit Limit]) values ('" & txtCustomerID.Text & "' , '" & txtFullnames.Text & "' , '" & txtAddress.Text & "' ,'" & txtBusinessName.Text & "' , '" & txtLocation.Text & "','" & txtCreditlimit.Text & "')"
+        Dim cmd As OleDbCommand = New OleDbCommand(command, myconnection)
+        cmd.Parameters.Add(New OleDbParameter("CustomerID", CType(txtCustomerID.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Full Names", CType(txtFullnames.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Address", CType(txtAddress.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Business Name", CType(txtBusinessName.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Location", CType(txtLocation.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Credit Limit", CType(txtCreditlimit.Text, String)))
+
+        Try
+            cmd.ExecuteNonQuery()
+            cmd.Dispose()
+            myconnection.Close()
+            txtCustomerID.Clear()
+            txtFullnames.Clear()
+            txtAddress.Clear()
+            txtBusinessName.Clear()
+            txtLocation.Clear()
+            txtCreditlimit.Clear()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
